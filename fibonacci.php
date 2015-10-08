ee84763fdae5c86812ee3fce7fb5fda598f6e6c4
ckitz <ckitz@meetlikeminds.io>
2015-10-07 21:21:38 -0500

    failure

--- fibonacci.php
+++ fibonacci.php
@@ -1,5 +1,13 @@
 <?php
 
-""
+class fibbonacci 
+{
+	function fibbonacci($i; $i<100; $i ++)
+	{
+		echo $i+5;
+		
+	}
+}
 
+$i=0
 ?>
--- triangles.php
+++ triangles.php
@@ -1,16 +1,47 @@
 <?php
-$n=5
-$x=0
-$y=0
+//$n=5
+//$x=0
+//$y=0
 
-while($x < $n)
-{
+//while($x < $n)
+//{
 
-while ($y < $n)
-{
+//}
+//while ($y < $n)
+//{
 
+//}
+
+//beckys function
+
+class shapemaker {
+	function shapemaker ($n)
+	{
+	for ($i= 0; $this->$j; $i ++)
+		{
+	for ($j= 0; $this->$j=$i; $j ++)
+		
+	{echo $this->newarray[$i][$j];
+	}
+	}
+	echo "\n";
+	}
+}
+function triangle() 
+	{
+for ($i = 0; $i < $this->input; $i++)
+	{
+		for ($j = 0; $j <($i+1); $j++)
+			
+		{echo $this->newarray[$i][$j];
+	}
+			echo "\n";
+	}
 }
 
+$n=4;
+echo $newarray["."]["."];
 
-}
+
+
 ?>
